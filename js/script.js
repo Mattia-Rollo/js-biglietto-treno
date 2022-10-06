eeb28bcfa51575c779ecf7f0aa05a4e3e3c64ac7
mattia.rollo <ste.tia@hotmail.it>
2022-10-06 14:47:09 +0200

    add index.html

---
+++ js/script.js
@@ -0,0 +1,55 @@
+/*
+x chiedere all'utente quanti km vuole percorrere
+x chiedere all'utente l'eta
+
+x convertire i dati inseriti da stringhe a numeri così da poter fare i calcoli
+
+x calcolare il costo del biglietto in base ai km inseriti 
+x dall'utente * 0.21
+x utente ha meno di 18 anni?
+x    applico lo sconto del 20%
+x utente ha più di 65 anni?
+x    applico lo sconto del 40%;
+x altrimenti lascio il prezzo invariato
+
+*/
+
+let kmPercorso = parseInt(prompt('quanti km dei percorrere?'));
+let eta = parseInt(prompt('quanti anni hai?'));
+console.log(kmPercorso, eta);
+
+let prezzoBiglietto = kmPercorso * 0.21;
+console.log(prezzoBiglietto);
+
+if (isNaN(kmPercorso) || isNaN(eta)) {
+    alert('inserisci solo numeri grazie');
+}
+
+if (eta < 18) {
+
+    prezzoBiglietto = prezzoBiglietto * 0.8; 
+    //alternativa --> prezzoBiglietto - prezzoBiglietto * 20 / 100;
+}else if (eta > 65) {
+    prezzoBiglietto = prezzoBiglietto * 0.6;
+}
+
+console.log(prezzoBiglietto.toFixed(2));
+
+let myH2 = document.getElementsByClassName('prezzo-biglietto')[0];
+
+if (isNaN(prezzoBiglietto)) {
+    
+    myH2.innerHTML = `
+    
+    il prezzo del biglietto è incalcolabile, metti solo numeri
+    
+    `
+}else {
+    myH2.innerHTML = `
+    
+    il prezzo del biglietto è ${prezzoBiglietto}
+    
+    `
+}
+
+
