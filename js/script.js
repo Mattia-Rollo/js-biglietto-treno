5ace0dfcaf6756fe7b70627efc9871feb3bb8b57
mattia.rollo <ste.tia@hotmail.it>
2022-10-06 21:51:19 +0200

    change style

--- js/script.js
+++ js/script.js
@@ -13,20 +13,22 @@ x    applico lo sconto del 40%;
 x altrimenti lascio il prezzo invariato
 
 */
-const prezzoKm = 0.21;
+let prezzoKm = 0.21;
 let kmPercorso = parseInt(prompt('quanti km devi percorrere?'));
 let eta = parseInt(prompt('quanti anni hai?'));
 console.log(kmPercorso, eta);
-let totale = 0;
-console.log(typeof totale);
-let sconto;
+
 
 let prezzoBiglietto = kmPercorso * prezzoKm;
 console.log(prezzoBiglietto);
 
-const maggiorenne = 18;
+let totale;
+console.log(typeof totale);
+let sconto;
+
+let maggiorenne = 18;
 console.log(typeof maggiorenne);
-const anziano = 65;
+let anziano = 65;
 
 if (isNaN(kmPercorso) || isNaN(eta)) {
     alert('inserisci solo numeri grazie');
@@ -34,11 +36,13 @@ if (isNaN(kmPercorso) || isNaN(eta)) {
 
 if (eta < maggiorenne) {
     totale = prezzoBiglietto * 0.8;
+    console.log(typeof totale);
     sconto = '20%'; 
     //alternativa --> prezzoBiglietto - prezzoBiglietto * 20 / 100;
 }else if (eta > anziano) {
     totale = prezzoBiglietto * 0.6;
     sconto = '40%';
+    console.log(typeof totale);
 }
 
 console.log('il prezzo è di ' + prezzoBiglietto.toFixed(2));
@@ -54,6 +58,8 @@ if (isNaN(prezzoBiglietto)) {
     
     
     `
+
+
     myH2.style.width = '600px';
     document.querySelector('h1').innerHTML = '<span class="">FATAL ERROR </span>';
     document.body.style.backgroundImage = "url('../terminator.jpg')" ;
@@ -61,17 +67,6 @@ if (isNaN(prezzoBiglietto)) {
 
 
 }
-// else if ((isNaN(!prezzoBiglietto)) && ((age <= 65) &&  (age >=18))) {
-//     myH2.innerHTML = `
-    
-//     Il prezzo del biglietto è di: ${totale.toFixed(2)} €
-    
-//     `;
-//     document.getElementById("MyElement").classList.remove('d-none');
-    
-
-// }
-
 else {
     if ( eta < 18) {
     console.log(totale);
@@ -80,7 +75,7 @@ else {
     
     Il prezzo del biglietto è di: ${prezzoBiglietto.toFixed(2)} €
     ma siccome hai ${eta} anni hai diritto a uno sconto del ${sconto}
-    quindi il totale è ${totale.toFixed(2)} €
+    quindi il totale è di: ${totale.toFixed(2)} €
     
     `;
     document.getElementById("MyElement").classList.remove('d-none');
@@ -92,13 +87,13 @@ else {
     
     Il prezzo del biglietto è di: ${prezzoBiglietto.toFixed(2)} €
     ma siccome hai ${eta} anni hai diritto a uno sconto del ${sconto}
-    quindi il totale è ${totale.toFixed(2)} €
+    quindi il totale è di: ${totale.toFixed(2)} €
     
     `;
     document.getElementById("MyElement").classList.remove('d-none');
     } else {
         myH2.innerHTML = `
-        Il prezzo del biglietto è di: ${prezzoBiglietto.toFixed(2)}
+        Il prezzo del biglietto è di: ${prezzoBiglietto.toFixed(2)} €
         `
         document.getElementById("MyElement").classList.remove('d-none');
     }
